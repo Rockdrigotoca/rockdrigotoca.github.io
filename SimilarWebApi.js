dfccb0ce727ecc4ddf14bbc07a76e4fe62949da4
Rockdrigotoca <rockdrigotoca@mac.com>
2016-06-06 12:25:11 -0500

    changed WB api url

--- SimilarWebApi.js
+++ SimilarWebApi.js
@@ -17,7 +17,7 @@
 };
  
  myConnector.getTableData = function () {
-    var connectionUrl = "https://api.similarweb.com/v1/website/metroscubicos.com/total-traffic-and-engagement/visits?api_key={2536e00f4120a5c347aef0c849b97e47}&start_date=2016-05&end_date=2016-05&main_domain_only=false&granularity=monthly";
+    var connectionUrl = "https://api.similarweb.com/v1/website/metroscubicos.com/total-traffic-and-engagement/visits?api_key=2536e00f4120a5c347aef0c849b97e47&start_date=2016-01&end_date=2016-04&main_domain_only=false&granularity=monthly";
 
     var xhr = $.ajax({
       url: getConnectionUrl(connectionUrl),
